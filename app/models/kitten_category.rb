bb693e57e53ada1a2bce893ad1c7bd60009ddd86
CaseyKelly <casey.kelly@colorado.edu>
2015-03-02 16:37:43 -0700

    User can't select same category twice for the same kitten

--- app/models/kitten_category.rb
+++ app/models/kitten_category.rb
@@ -2,5 +2,5 @@ class KittenCategory < ActiveRecord::Base
   belongs_to :kitten
   belongs_to :category
 
-  validates :category_id, presence: true
+  validates :category_id, presence: true, uniqueness: true
 end
