5f387cb90db19e32debf0f910906678e76b38e6f
CaseyKelly <casey.kelly@colorado.edu>
2015-03-02 15:11:55 -0700

    Users can create kitten categories

--- app/controllers/kitten_categories_controller.rb
+++ app/controllers/kitten_categories_controller.rb
@@ -1,6 +1,23 @@
 class KittenCategoriesController < ApplicationController
 
   def new
+    @kitten = Kitten.find(params[:kitten_id])
+    @kitten_category = KittenCategory.new
   end
 
+  def create
+    @kitten = Kitten.find(params[:kitten_id])
+    if current_user
+      @kitten_category = KittenCategory.new
+      @kitten_category.kitten_id = @kitten.id
+      @kitten_category.category_id = params[:kitten_category][:category_id]
+      if @kitten_category.save
+        redirect_to root_url
+      else
+        render :new
+      end
+    else
+      redirect_to login_path, notice: "Must be logged in to review a company"
+    end
+  end
 end
