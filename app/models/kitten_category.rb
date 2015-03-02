423e686fb9a2308ebb3f6530b6ccdb51bd8d3dc5
CaseyKelly <casey.kelly@colorado.edu>
2015-03-02 16:58:40 -0700

    Create custom validation for multiple category ids

--- app/models/kitten_category.rb
+++ app/models/kitten_category.rb
@@ -2,5 +2,12 @@ class KittenCategory < ActiveRecord::Base
   belongs_to :kitten
   belongs_to :category
 
-  validates :category_id, presence: true, uniqueness: true
+  validates :category_id, presence: true
+  validate :unique_ids
+
+ def unique_ids
+   if KittenCategory.find_by(category_id: category_id, kitten_id: kitten_id)
+     errors.add(:base, 'Category already exists for this kitten')
+   end
+ end
 end
